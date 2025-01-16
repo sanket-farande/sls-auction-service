d78a2c43a8cbecd105d17a1af9b2edb9830704e1
54NK37 <sanketfaraned2@gmail.com>
2025-01-16 21:59:16 +0530

    Added dynamo db create and insert

--- createAuction.js
+++ createAuction.js
@@ -1,16 +1,27 @@
-exports.createAuction = async (event) => {
-  const {title} = JSON.parse(event.body);
+import { v4 } from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+export const createAuction = async (event) => {
+  const { title } = JSON.parse(event.body);
   const now = new Date();
 
   const auction = {
+    id: v4(),
     title,
     status: 'OPEN',
     createdAt: now.toISOString()
   }
 
+  await dynamoDB.put({
+    TableName: 'AuctionsTable',
+    Item: auction
+  }).promise();
+
   // 201: Resource created
   return {
     statusCode: 201,
-    body: JSON.stringify({auction})
+    body: JSON.stringify({ auction })
   };
 };
