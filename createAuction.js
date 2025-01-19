4c876ecb043ff0ccbd1048aadb2b4682f0d0bd89
54NK37 <sanketfaraned2@gmail.com>
2025-01-19 19:51:02 +0530

    Added middy middlewares and error handling

--- createAuction.js
+++ createAuction.js
@@ -1,10 +1,16 @@
 import { v4 } from 'uuid';
 import AWS from 'aws-sdk';
+import middy from '@middy/core'
+import httpJsonBosyParser from '@middy/http-json-body-parser'
+import httpEventNormalizer from '@middy/http-event-normalizer'
+import httpEventHandler from '@middy/http-error-handler'
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors';
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-export const createAuction = async (event) => {
-  const { title } = JSON.parse(event.body);
+const createAuction = async (event) => {
+  const { title } = event.body;
   const now = new Date();
 
   const auction = {
@@ -14,11 +20,16 @@ export const createAuction = async (event) => {
     createdAt: now.toISOString()
   }
 
-  await dynamoDB.put({
-    // Environment variable
-    TableName: process.env.AUCTIONS_TABLE_NAME,
-    Item: auction
-  }).promise();
+  try {
+    await dynamoDB.put({
+      // Environment variable
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: auction
+    }).promise();
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 
   // 201: Resource created
   return {
@@ -26,3 +37,11 @@ export const createAuction = async (event) => {
     body: JSON.stringify({ auction })
   };
 };
+
+export const handler = middy(createAuction)
+  // Stringified body obj 
+  .use(httpJsonBosyParser())
+  // Prevents from Cannot read properties of undefined error by adding those objects
+  .use(httpEventNormalizer())
+  // Error Handling
+  .use(httpErrorHandler())
