a79dd8399003806df00cf819c3509d156522cdd2
54NK37 <sanketfaraned2@gmail.com>
2025-01-26 17:21:53 +0530

    Marking auctions as Closed

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,11 +1,14 @@
 import { catchBlockCode } from "auction-service-common";
 import { getEndedAuctions } from "../lib/getEndedAuctions";
+import { closeAuction } from "../lib/closeAuction";
 
 async function processAuctions(event) {
     try {
         console.log(`Processing Auctions!`);
         const auctionsToClose = await getEndedAuctions();
-        console.log(auctionsToClose);
+        const promises = auctionsToClose.map((auction) => closeAuction(auction));
+        await Promise.all(promises);
+        return { closed: promises.length };
     } catch (error) {
         catchBlockCode(error);
     }
