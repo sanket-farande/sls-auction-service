764d2375c4451238ca381bf0c5e45e7989bba262
54NK37 <sanketfaraned2@gmail.com>
2025-03-07 20:54:15 +0530

    Indent fix

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -3,15 +3,15 @@ import { getEndedAuctions } from "../lib/getEndedAuctions";
 import { closeAuction } from "../lib/closeAuction";
 
 async function processAuctions(event) {
-    try {
-        console.log(`Processing Auctions!`);
-        const auctionsToClose = await getEndedAuctions();
-        const promises = auctionsToClose.map((auction) => closeAuction(auction));
-        await Promise.all(promises);
-        return { closed: promises.length };
-    } catch (error) {
-        catchBlockCode(error);
-    }
+  try {
+    console.log(`Processing Auctions!`);
+    const auctionsToClose = await getEndedAuctions();
+    const promises = auctionsToClose.map((auction) => closeAuction(auction));
+    await Promise.all(promises);
+    return { closed: promises.length };
+  } catch (error) {
+    catchBlockCode(error);
+  }
 }
 
 export const handler = processAuctions;
